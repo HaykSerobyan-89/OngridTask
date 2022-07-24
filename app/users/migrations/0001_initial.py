2a2bb281eed7546e0a06563616a22066c8e79999
HaykSerobyan-arpify <haykserobyan@arpify.com>
2022-07-24 23:14:29 +0400

    add markdown file

--- app/users/migrations/0001_initial.py
+++ app/users/migrations/0001_initial.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -17,14 +16,23 @@ class Migration(migrations.Migration):
             fields=[
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('is_superuser', models.BooleanField(default=False,
+                                                     help_text='Designates that this user has all permissions '
+                                                               'without explicitly assigning them.',
+                                                     verbose_name='superuser status')),
                 ('id', models.AutoField(editable=False, primary_key=True, serialize=False)),
                 ('email', models.EmailField(max_length=255, unique=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('is_active', models.BooleanField(default=False)),
                 ('is_staff', models.BooleanField(default=False)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True,
+                                                  help_text='The groups this user belongs to. A user will get all '
+                                                            'permissions granted to each of their groups.',
+                                                  related_name='user_set', related_query_name='user', to='auth.group',
+                                                  verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                                                            related_name='user_set', related_query_name='user',
+                                                            to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
---
+++ readme.md
@@ -0,0 +1,29 @@
+<h1 align="center"> :zap: Ongrid Task :zap:</h1>
+
+![](https://img.shields.io/badge/Python-3.9.7-brightgreen) 
+![](https://img.shields.io/badge/Django-4.0.6-brightgreen)
+![](https://img.shields.io/badge/yarn-1.22.19-blue)
+![](https://img.shields.io/badge/Node-v16.15.1-blue)
+
+![](https://img.shields.io/badge/django%20rest-ff1709?style=for-the-badge&logo=django&logoColor=white)
+![](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
+![](https://img.shields.io/badge/Docker-2CA5E0?style=for-the-badge&logo=docker&logoColor=white)
+
+
+## *Task description link*
+> https://docs.google.com/document/d/1NuPLHv-7XN9YjVXFLNDDqhbOvH_eIhhxZ8TX_qtuT9g/edit#
+## *Installation*
+### ***Run With Docker Container***
+`docker-compose up --build -d`
+### *Run from terminal*
+#### *To run Django Backend App*
+```commandline
+pip3 install -r requirements.txt
+cd app && python3 manage.py runserver
+```
+
+Open new terminal to run React Frontend App
+```commandline
+yarn install
+yarn start
+```
